d82fa1412b46f5a5bb4b911ff0680daa20a0a39d
ARahim-S <arahim.s@yandex.com>
2023-05-05 20:29:02 +0300

    update

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+// import {} from "./reducers/"
+
+const Store = configureStore({
+  rducers: {},
+});
+
+export default Store;
+
+//3:54 te kaldım
