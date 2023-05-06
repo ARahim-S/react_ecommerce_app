609469b74dcc103709968428e2f32e48de77fea1
ARahim-S <arahim.s@yandex.com>
2023-05-06 14:04:21 +0300

    updated

--- frontend/src/components/Layout/Header.jsx
+++ frontend/src/components/Layout/Header.jsx
@@ -3,10 +3,13 @@ import styles from "../../styles/styles";
 import { Link } from "react-router-dom";
 import { productData } from "../../static/data";
 import { AiOutlineSearch } from "react-icons/ai";
+import { IoIosArrowForward } from "react-icons/io";
+import { BiMenuAltLeft } from "react-icons/bi";
 
 const Header = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [searchData, setSearchData] = useState([]);
+  const [active, setActive] = useState(false);
 
   const handleSearchChange = (e) => {
     const term = e.target.value;
@@ -23,53 +26,90 @@ const Header = () => {
       setSearchData([]);
     }
   };
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 70) {
+      setActive(true);
+    } else {
+      setActive(false);
+    }
+  });
+
   return (
-    <div className={`${styles.section}`}>
-      <div className="hidden 800px:h-[50px] 800px:my-[20px] 800px:flex items-center justify-between">
-        <div>
-          <Link to={"/"}>
-            <img
-              src="https://shopo.quomodothemes.website/assets/images/logo.svg"
-              alt="logo"
+    <>
+      <div className={`${styles.section}`}>
+        <div className="hidden 800px:h-[50px] 800px:my-[20px] 800px:flex items-center justify-between">
+          <div>
+            <Link to={"/"}>
+              <img
+                src="https://shopo.quomodothemes.website/assets/images/logo.svg"
+                alt="logo"
+              />
+            </Link>
+          </div>
+          {/* Search box */}
+          <div className="w-[50%] relative">
+            <input
+              type="text"
+              placeholder="Search Product..."
+              value={searchTerm}
+              onChange={handleSearchChange}
+              className="h-[40px] w-full px-2 border-[#3957db] border-[2px] rounded-md"
             />
-          </Link>
-        </div>
-        {/* Search box */}
-        <div className="w-[50%] relative">
-          <input
-            type="text"
-            placeholder="Search Product..."
-            value={searchTerm}
-            onChange={handleSearchChange}
-            className="h-[40px] w-full px-2 border-[#3957db] border-[2px] rounded-md"
-          />
-          <AiOutlineSearch
-            size={30}
-            className="absolute right-2 top-1.5 cursor-pointer"
-          />
-          {searchData?.length !== 0 ? (
-            <div className="absolute min-h-[30vh] bg-slate-50 shadow-sm-2 z-9 p-9 ">
-              {searchData?.map((item, index) => {
-                const d = item.name;
-                const productName = d.replace(/\s+/g, "-");
-                return (
-                  <Link to={`/product/${productName}`} key={index}>
-                    <div className="w-full flex items-start-py-3">
-                      <img
-                        src={item.image_Url[0].url}
-                        alt=""
-                        className="w-[40px] h-[40px] mr-[10px]"
-                      />
-                      <h1>{item.name}</h1>
-                    </div>
-                  </Link>
-                );
-              })}
-            </div>
-          ) : null}
+            <AiOutlineSearch
+              size={30}
+              className="absolute right-2 top-1.5 cursor-pointer"
+            />
+            {searchData?.length !== 0 ? (
+              <div className="absolute min-h-[30vh] bg-slate-50 shadow-sm-2 z-9 p-9 ">
+                {searchData?.map((item, index) => {
+                  const d = item.name;
+                  const productName = d.replace(/\s+/g, "-");
+                  return (
+                    <Link to={`/product/${productName}`} key={index}>
+                      <div className="w-full flex items-start-py-3">
+                        <img
+                          src={item.image_Url[0].url}
+                          alt=""
+                          className="w-[40px] h-[40px] mr-[10px]"
+                        />
+                        <h1>{item.name}</h1>
+                      </div>
+                    </Link>
+                  );
+                })}
+              </div>
+            ) : null}
+          </div>
+          <div className={`${styles.button}`}>
+            <Link to={"/seller"}>
+              <h1 className="text-[#fff] flex items-center">
+                Become Seller <IoIosArrowForward className="ml-1" />
+              </h1>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+      <div
+        className={`${
+          active === true ? "shadow-md fixed top-0 left-0" : null
+        } transition hidden 800px:flex items-center justify-between w-full bg-[#3321c8] h-[70px]`}
+      >
+        <div
+          className={`${styles.section} relative ${styles.noramlFlex} justify-between`}
+        >
+          {/* Categories*/}
+          <div>
+            <div className="relative h-[60px] mt-[10px] w-[270px] hidden 1000px:block">
+              <BiMenuAltLeft size={30} className="absolute top-3 left-2" />
+              <button className="h-[100%] w-full flex justify-between items-center pl-10 bg-white font-sans text-lg font-[500] select-none rounded-t-md ">
+                All categories
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
